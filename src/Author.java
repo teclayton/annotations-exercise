7645a8d6014c6584d23f5273b986eeaf9595e84c
Tony Clayton <te.clayton@yahoo.com>
2019-04-02 10:46:41 -0500

    fixed various warnings

--- src/Author.java
+++ src/Author.java
@@ -15,15 +15,16 @@ public class Author extends Person {
     /**
      * @deprecated Use publishedBooks instead
      */
+    @SuppressWarnings("unchecked")
     @Deprecated
     public List<String> getBooks() {
         return books;
     }
-
+    @SuppressWarnings("unchecked")
     public List<String> publishedBooks() {
         return books;
     }
-
+    @SuppressWarnings("unchecked")
     public void addBook(String book) {
         books.add(book);
     }
